053580b4aef61f7d16da6231a2695cc2de6eda4c
3038ksh <3038ksh@naver.com>
2018-01-18 17:45:40 +0900

    module-remaster

--- p2p.py
+++ p2p.py
@@ -1,183 +1,65 @@
 import json
-import rsa
 from urllib.parse import urlparse
 from uuid import uuid4
 
 import requests
-from flask import Flask, jsonify, request
-from threading import Thread
-import signal
-from time import sleep
 
-import blockchain
+# nodes_addr = ["http://192.168.0.20:5000",
+#               "http://192.168.0.22:5000"]
 
-# Instantiate the Node
-app = Flask(__name__)
+nodes_addr = ["http://127.0.0.1:5002",
+              "http://127.0.0.1:5000",
+              "http://127.0.0.1:5001"]
 
-# Generate a globally unique address for this node
-node_identifier = str(uuid4()).replace('-', '')
+leader_id = None
+leader_idx = -1
+nodes = []
 
-# Generate a pair of the public key and the private key
-(pubkey, privkey) = rsa.newkeys(512)
+def get_info():
+    flag = True
 
-# Instantiate the Blockchain
-blockchain = blockchain.Blockchain()
+    for node in nodes_addr:
+        url = node + '/id'
+        response = requests.get(url=url)
+        node_id, node_pubkey = None, None
 
+        if response.status_code == 200:
+            node_id = response.json()['id']
 
-@app.route('/mine', methods=['GET'])
-def mine():
-    # We run the proof of work algorithm to get the next proof...
-    last_block = blockchain.last_block
-    last_proof = last_block['proof']
-    proof = blockchain.proof_of_work(last_proof)
+        url = node + '/pubkey'
+        response = requests.get(url=url)
 
-    # We must receive a reward for finding the proof.
-    # The sender is "0" to signify that this node has mined a new coin.
-    blockchain.new_transaction(
-        sender="0",
-        recipient=node_identifier,
-        amount=1,
-    )
+        if response.status_code == 200:
+            node_pubkey = response.json()['pubkey']
 
-    # Forge the new Block by adding it to the chain
-    previous_hash = blockchain.hash(last_block)
-    block = blockchain.new_block(proof, previous_hash)
+        nodes.append({'id': node_id, 'address': node, 'pubkey': node_pubkey, 'leader': flag})
+        if flag == True:
+            global leader_id
+            leader_id = node_id
 
-    response = {
-        'message': "New Block Forged",
-        'index': block['index'],
-        'transactions': block['transactions'],
-        'proof': block['proof'],
-        'previous_hash': block['previous_hash'],
-    }
-    return jsonify(response), 200
+        flag = False
 
+def send_info():
+    data = {'nodes': nodes, 'leader_idx': leader_idx}
+    jdata = json.dumps(data)
+    headers = {'Content-Type': 'application/json'}
 
-@app.route('/transactions/new', methods=['POST'])
-def new_transaction():
-    values = request.get_json()
+    for node in nodes_addr:
+        url = node + '/nodes/register'
+        response = requests.post(url, headers=headers, data=jdata)
 
-    # Check that the required fields are in the POST'ed data
-    required = ['sender', 'recipient', 'amount']
-    if not all(k in values for k in required):
-        return 'Missing values', 400
+        if response.status_code == 200:
+            msg = response.json()['message']
+            total_nodes = response.json()['total_nodes']
+            print(msg)
+            for t in total_nodes:
+                print(t)
 
-    # Create a new Transaction
-    index = blockchain.new_transaction(values['sender'], values['recipient'], values['amount'])
+get_info()
 
-    response = {'message': f'Transaction will be added to Block {index}'}
-    return jsonify(response), 201
+nodes = sorted(nodes, key=lambda k:k['id'])
+for i in range(len(nodes)):
+    if nodes[i].get('id') == leader_id:
+        leader_idx = i
 
-
-# @app.route('/transactions/new', methods=['POST'])
-# def new_transaction():
-#     values = request.get_json()
-
-#     # Check that the required fields are in the POST'ed data
-#     required = ['sender', 'recipient', 'amount']
-#     if not all(k in values for k in required):
-#         return 'Missing values', 400
-
-#     # Create a new Transaction
-#     index = blockchain.new_transaction(values['sender'], values['recipient'], values['amount'])
-
-#     response = {'message': f'Transaction will be added to Block {index}'}
-#     return jsonify(response), 201
-
-
-@app.route('/chain', methods=['GET'])
-def full_chain():
-    response = {
-        'chain': blockchain.chain,
-        'length': len(blockchain.chain),
-    }
-    return jsonify(response), 200
-
-
-@app.route('/id', methods=['GET'])
-def get_id():
-    response = {'id': f'{node_identifier}'}
-    return jsonify(response), 200
-
-
-@app.route('/pubkey', methods=['GET'])
-def get_pubkey():
-    str_pubkey = str(pubkey)
-    parse_pubkey = str_pubkey.split('(')[1][:-1].split(' ')
-    parse_num = {}
-    parse_num['e'] = int(parse_pubkey[1])
-    parse_num['n'] = int(parse_pubkey[0][:-1])
-
-    response = {'pubkey': parse_num}
-    return jsonify(response), 200
-
-
-@app.route('/connected_nodes', methods=['GET'])
-def get_connected_nodes():
-    response = {'nodes': list(blockchain.nodes.keys())}
-    return jsonify(response), 200
-
-
-@app.route('/nodes/register', methods=['POST'])
-def register_nodes():
-    values = request.get_json()
-
-    nodes = values.get('nodes')
-    leader_idx = values.get('leader_idx')
-    blockchain.leader_idx = leader_idx
-
-    if nodes is None:
-        return "Error: Please supply a valid list of nodes", 400
-
-    for node in nodes:
-        node_id = node.get('id')
-        node_addr = node.get('address')
-        node_pubkey = rsa.PublicKey(**node.get('pubkey'))
-        is_leader = node.get('leader')
-
-        if is_leader is True:
-            blockchain.leader = (node_id, node_addr)
-
-        if node_id != node_identifier:
-            blockchain.register_node(node_id, node_addr, pubkey)
-
-    response = {
-        'message': 'New nodes have been added',
-        'total_nodes': list(blockchain.nodes.keys()),
-    }
-    return jsonify(response), 201
-
-
-@app.route('/nodes/resolve', methods=['GET'])
-def consensus():
-    replaced = blockchain.resolve_conflicts()
-
-    if replaced:
-        response = {
-            'message': 'Our chain was replaced',
-            'new_chain': blockchain.chain
-        }
-    else:
-        response = {
-            'message': 'Our chain is authoritative',
-            'chain': blockchain.chain
-        }
-
-    return jsonify(response), 200
-
-
-@app.route('/nodes/leader', methods=['GET'])
-def get_leader():
-    response = {'nodes': blockchain.leader}
-    return jsonify(response), 200
-
-
-if __name__ == '__main__':
-    from argparse import ArgumentParser
-
-    parser = ArgumentParser()
-    parser.add_argument('-p', '--port', default=5000, type=int, help='port to listen on')
-    args = parser.parse_args()
-    port = args.port
-
-    app.run(host='127.0.0.1', port=port)
+send_info()
