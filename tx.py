cb0c8a34ceb65b0b9b75c894db83220d6e34a8f9
3038ksh <3038ksh@naver.com>
2018-02-02 18:02:56 +0900

    tx test bug fix2

--- tx.py
+++ tx.py
@@ -58,7 +58,7 @@ def send_tx():
     for utxo in utxo_list:
         for node in nodes:
             url = node.get('address') + '/transactions/new'
-            data = {'rand_id': utxo, 'candidate': candidate[random.randint(0, 2)]}
+            data = {'rand_id': utxo, 'candidate': candidate_list[random.randint(0, 2)]}
             node_pubkey = rsa.PublicKey(**node.get('pubkey'))
             cdata = rsa.encrypt(str(data).encode('utf8'), node_pubkey)
             sign = rsa.sign(cdata, prikey, 'SHA-1')
